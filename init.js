3a5cbf454761e43cef0e9c09d7e891ed55e74d23
selekta <runsense974@gmail.com>
2015-09-04 17:33:24 +0400

    accelerate

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
 $.urlParam=function(name){var results=new RegExp('[\?&amp;]' + name + '=([^&amp;#]*)').exec(window.location.href);try{return results[1]||0;}catch(e){return null;}}
 $(window).resize(function () { var h = $(window).height();var offsetTop = 105; $(FuncInit.idmap).css('height', (h - offsetTop));}).resize();
 $(function(){FuncInit.init();FuncTree.init();if(FuncInit.bxt) FuncInit.tmp="OUEST";MapsLib.initialize();if (typeof google !== 'object' && typeof google.maps !== 'object') {location.reload(false);} });
-if(!FuncInit.bnm){$(FuncInit.idrbb).append("<button >enter</button>");}
+if(!FuncInit.bnm){$("#formob").append("<button >enter</button>");}$(FuncInit.idinf).append("If Not reload!!<span>Si Aucune carte, <b>recharger</b><h4><i>trop fast for navigator</i></span>");
