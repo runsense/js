c9a57df779dceb4a2ebf81118d34bc253447c4ad
selekta <runsense974@gmail.com>
2015-09-07 15:19:30 +0400

    end of version 1

--- init.js
+++ init.js
@@ -1,3 +1,3 @@
-$.urlParam=function(name){var results=new RegExp('[\?&amp;]' + name + '=([^&amp;#]*)').exec(window.location.href);try{return results[1]||0;}catch(e){return null;}}
 $(window).resize(function () { var h = $(window).height();var offsetTop = 105; $(FuncInit.idmap).css('height', (h - offsetTop));}).resize();
 $(function(){FuncInit.init();FuncTree.init();if(FuncInit.bxt) FuncInit.tmp="OUEST";MapsLib.initialize();if (typeof google !== 'object' && typeof google.maps !== 'object') {location.reload(false);} });
+google.maps.visualRefresh = true;
