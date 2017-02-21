86958707dd02685e36b2e9b07f604cebb2081231
pascal d <pascal d>
2017-02-21 21:19:33 +0400

    creation

--- js/buy_lib.js
+++ js/buy_lib.js
@@ -1,76 +1,54 @@
-
-
-
-http://webservices.amazon.com/onca/xml?
-Service=AWSECommerceService&
-Operation=ItemSearch(SimilarityLookup,ItemLookup)&
-AWSAccessKeyId=AKIAJ3VHSVKHX4WZVECA&
-AssociateTag=rICB9qNUTtahGA0+H+7qozsF+koYLE2g7u+hyRSA&
-SearchIndex=FashionMen&
-Sort=price&
-Keywords=lacoste%20polo&
-ResponseGroup=Accessories%2CImages%2CItemAttributes
-&Timestamp=2017-02-15T12%3A00%3A00Z
-&Signature=[Request Signature]
-
-signature:
-
-query='
-AWSAccessKeyId=AKIAJ3VHSVKHX4WZVECA
-&AssociateTag=rICB9qNUTtahGA0+H+7qozsF+koYLE2g7u+hyRSA
-&ItemId=0679722769
-&Operation=ItemLookup
-&ResponseGroup=Images%2CItemAttributes%2COffers%2CReviews....pour Images,ItemAttributes,Offers,Reviews    http://docs.aws.amazon.com/AWSECommerceService/latest/DG/CHAP_ResponseGroupsList.html
-&Service=AWSECommerceService
-&Timestamp=Date.toISOString().replace(/:/g,'%3A')
-&Version=2013-08-01'
-
-make_request=function(query,marketPlace) {
-    hash_data = ['GET',marketPlace, '/onca/xml', query].join("\n")
-    secret = "1234567890"
-    hmac = hmac_(hash_data, secret)
-    var sign = $.base64.encode(hmac),
-    console.log("js_sign="+sign);
-	$.ajax(query:'http://webservices.amazon.com/onca/xml?'+query+'&Signature='+sign);
+$.urlParam=function(name){var results=new RegExp('[\?&]'+name+'=([^&#]*)').exec(window.location.href);try{return results[1]||0;}catch(e){return null;}};
+var A=A||{};
+var A={ak:'AKIAI5LRTOJW7RFXOMEA',
+id:'',
+m:'FR',
+dt:new Date().toISOString(),
+mag:function(m){switch(m){case 'BR':return 'http://webservices.amazon.com.br/onca/xml';case 'bCA':return 'http://webservices.amazon.ca/onca/xml';
+case 'CN':return 'http://webservices.amazon.cn';
+case 'DE':return 'http://webservices.amazon.de';
+case 'ES':return 'http://webservices.amazon.es';
+case 'FR':return 'http://webservices.amazon.fr';
+case 'IN':return 'http://webservices.amazon.in';
+case 'IT':return 'http://webservices.amazon.it';
+case 'JP':return 'http://webservices.amazon.co.jp';
+case 'MX':return 'http://webservices.amazon.com.mx';
+case 'UK':return 'http://webservices.amazon.co.uk';
+case 'US':return 'http://webservices.amazon.com';
+default:break;}},
+itmFc:{a:'ItemSearch',b:'SimilarityLookup',c:'ItemLookup'},
+srcEng:{a:'SearchIndex=FashionMen&Keywords=',b:'&ItemId='},
+tx:'reunion'};
+var F=F||{};
+var F={
+query:'AWSAccessKeyId='+A.ak+'&AssociateTag=rICB9qNUTtahGA0+H+7qozsF+koYLE2g7u+hyRSA&'+A.srcEng.a+A.tx+'&Sort=price&Keywords='+A.tx+'&ResponseGroup=Accessories%2CImages%2CItemAttributes&Timestamp='+A.dt.replace(/:/g,'%3A'),
+srch:function(query,mkp) {
+    hash_data = ['GET',mkp, '/onca/xml', query].join("\n")
+    secret = "xfKMIJlIqrwNSRtYX+a7MrPw8wQh/cWBQq+rABEX"
+    hmac=F.hmac_(hash_data, secret);
+    var sign=$.base64.encode(hmac);
+    console.log("js_sign="+sign);	
+	alert(mkp+'?Service=AWSECommerceService&Operation='+A.itmFc.a+'&'+F.query+'&Signature='+sign);
+	$.ajaxSetup({
+	headers:{   
+		'Access-Control-Allow-Origin: *':mkp
+	  }
+	});
+$.ajax({url:mkp+'?Service=AWSECommerceService&Operation='+A.itmFc.a+'&'+F.query+'&Signature='+sign,
+	success:F.load});
+	},
+hmac_:function(message,secret) {
+		var newHMAC=CryptoJS.HmacSHA256(message,secret);
+		return newHMAC;
+	},
+load:function(data,statut,xhr){
+	alert(data);
+	$('#'+A.m).append(data);
 }
-hmac_=function(message,secret) {
-var newHMAC = CryptoJS.HmacSHA256(message,secret);
-    return newHMAC;
 }
 
-Locale	Endpoint, marketPlace
-BR	
-http://webservices.amazon.com.br/onca/xml
+F.srch($.urlParam('tx'),A.mag(A.m));
 
-CA	
-http://webservices.amazon.ca/onca/xml
 
-CN	
-http://webservices.amazon.cn/onca/xml
 
-DE	
-http://webservices.amazon.de/onca/xml
 
-ES	
-http://webservices.amazon.es/onca/xml
-
-FR	
-http://webservices.amazon.fr/onca/xml
-
-IN	
-http://webservices.amazon.in/onca/xml
-
-IT	
-http://webservices.amazon.it/onca/xml
-
-JP	
-http://webservices.amazon.co.jp/onca/xml
-
-MX	
-http://webservices.amazon.com.mx/onca/xml
-
-UK	
-http://webservices.amazon.co.uk/onca/xml
-
-US	
-http://webservices.amazon.com/onca/xml
