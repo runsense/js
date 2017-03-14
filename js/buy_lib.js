268079c2412d346e1c73426fa6cb80c9a78728aa
pascal d <pascal d>
2017-03-14 15:57:00 +0400

    correct

--- js/buy_lib.js
+++ js/buy_lib.js
@@ -1,22 +1,48 @@
 $.urlParam=function(name){var results=new RegExp('[\?&]'+name+'=([^&#]*)').exec(window.location.href);try{return results[1]||0;}catch(e){return null;}};
-/*var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://runsense.au.auth0.com/oauth/token",
-  "method": "POST",
-  "headers": {
-    "content-type": "application/json"
-  },
-  "processData": false,
-  "data": "{\"grant_type\":\"client_credentials\",\"client_id\": \"kpDDBx1p1Kbl9cBnb6Ews4bvU2S6uDPV\",\"client_secret\": \"hoUWxmgBGCtvk58w4LO8-jWylQiVBQipa9Eot-o2zHmMWdMYb5yYlILyDTf8kGs0\",\"audience\": \"YOUR_API_IDENTIFIER\"}"
-}
+var A=A||{};
+var A={ak:'',
+s:'',
+id:'',
+tok:'',
+m:'FR',
+dt:new Date().toISOString(),
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});*/
-//aws
-//get credential
+mag:function(m){return m=='BR'?'webservices.amazon.com.br':m=='bCA'?'webservices.amazon.ca':m=='CN'?'webservices.amazon.cn':m=='DE'?'webservices.amazon.de':m=='ES'?'webservices.amazon.es':m=='FR'?'webservices.amazon.fr':m=='IN'?'webservices.amazon.in':m=='IT'?'webservices.amazon.it':m=='JP'?'webservices.amazon.co.jp':m=='MX'?'webservices.amazon.com.mx':m=='UK'?'webservices.amazon.co.uk':m=='US'?'webservices.amazon.com':'';
+},
+itmFc:{a:'ItemSearch',b:'SimilarityLookup',c:'ItemLookup'},
+srcEng:{a:'SearchIndex=Music&Keywords=',b:'&ItemId='},
+tx:$.urlParam('tx')};
 
+var bucket = new AWS.S3({
+
+            params: {
+
+                Bucket: 'runbuybucket'
+
+            }
+
+        });
+
+AWS.config.update({
+  region: 'ap-southeast-2',
+  credentials: new AWS.CognitoIdentityCredentials({
+    AccountId: '770434513982', // your AWS account ID
+    RoleArn: 'arn:aws:iam::770434513982:role/Cognito_auth0Unauth_Role',
+    IdentityPoolId:'ap-southeast-2:65b3cce2-eb19-42f5-a2af-28e031162916'
+  })
+});
+AWS.config.credentials.get(function(err) {
+  if (!err) {
+	 A.ak = AWS.config.credentials.accessKeyId;
+     A.s = AWS.config.credentials.secretAccessKey;
+    A.tok = AWS.config.credentials.identityToken;  
+    var id = AWS.config.credentials.identityId;
+	alert(A.ak+" "+A.s);
+	F.srch('sizzla',A.mag(A.m));
+    console.log("Cognito Identity Id:", id);
+  }else
+	  alert(err);
+});
 
 var lock =new Auth0Lock('kpDDBx1p1Kbl9cBnb6Ews4bvU2S6uDPV', 'runsense.au.auth0.com',{
         auth: {
@@ -26,22 +52,38 @@ var lock =new Auth0Lock('kpDDBx1p1Kbl9cBnb6Ews4bvU2S6uDPV', 'runsense.au.auth0.c
         }
     });
 	
-AWS.config.region = 'ap-southeast-2'; // Region
+AWS.config.region = 'ap-southeast-2'; 
 
 var btn_login = document.getElementById('btn-login');
 
 btn_login.addEventListener('click', function() {
   lock.show({auth: {params: {state: 'credAct'}}});
 });
-
-
+lock.on("authenticated", function(authResult) {
+	
+  lock.getProfile(authResult.idToken, function(error, profile) {
+    if (error) {
+	  alert("error"+error);
+      return;
+    }
+	alert('id_token'+authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem("accessToken", authResult.accessToken);
+    localStorage.setItem("profile", JSON.stringify(profile));
+		alert(JSON.stringify(profile));
+		$.ajax({url:'https://www.googleapis.com/fusiontables/v1/query?sql=SELECT nm,val FROM 1gUR93lfPRWJSNTnp0PmoYZzE7Hxpiurd7s0NXLjb&callback=F.ini&key=AIzaSyBGz-WlLTXCzhyqVkz-6N0QZQCV_HD7jdc',
+		dataType:'jsonp'});
+		credAct(authResult.accessToken);
+		
+  });
+});
 
 //identityId = credentialsProvider.getIdentityId();
 credAct=function(tok){
 	AWS.config.credentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: 'ap-southeast-2:65b3cce2-eb19-42f5-a2af-28e031162916',
 
-	  IdentityId: 'runBuilt-at-770434513982',
+	  //IdentityId: 'runBuilt-at-770434513982',
 
 	  // optional, only necessary when the identity pool is not configured
 	  // to use IAM roles in the Amazon Cognito Console
@@ -51,6 +93,7 @@ credAct=function(tok){
 		'runsense.au.auth0.com':tok
 	 }
 	});
+	//
 	//save user data
 	/*AWS.config.credentials.get(function(){
 
@@ -73,36 +116,25 @@ credAct=function(tok){
 	   });
 
 	});*/
-	F.srch('sizzla',A.mag(A.m),tok);//$.urlParam('tx')
+	
 }
 
 
-var A=A||{};
-var A={ak:'',
-s:'',
-id:'',
-m:'FR',
-dt:new Date().toISOString(),
 
-mag:function(m){return m=='BR'?'webservices.amazon.com.br':m=='bCA'?'webservices.amazon.ca':m=='CN'?'webservices.amazon.cn':m=='DE'?'webservices.amazon.de':m=='ES'?'webservices.amazon.es':m=='FR'?'webservices.amazon.fr':m=='IN'?'webservices.amazon.in':m=='IT'?'webservices.amazon.it':m=='JP'?'webservices.amazon.co.jp':m=='MX'?'webservices.amazon.com.mx':m=='UK'?'webservices.amazon.co.uk':m=='US'?'webservices.amazon.com':'';
-},
-itmFc:{a:'ItemSearch',b:'SimilarityLookup',c:'ItemLookup'},
-srcEng:{a:'SearchIndex=Music&Keywords=',b:'&ItemId='},
-tx:'sizzla'};
 
 var F=F||{};
 var F={
 query:'Service=AWSECommerceService&Operation='+A.itmFc.a+
 '&AWSAccessKeyId='+A.ak+'&AssociateTag=runbuy09-21&'+
 A.srcEng.a+A.tx+'&Sort=price&ResponseGroup=Accessories%2CImages%2CItemAttributes&Timestamp='+A.dt.replace(/:/g,'%3A'),
-srch:function(query,mkp,tok){
+srch:function(query,mkp){
 	A.tx=query;
 	F.query='AWSAccessKeyId='+A.ak+'&AssociateTag=mixyoutube-21&Keywords=sizzla&Operation=ItemSearch&SearchIndex=Music&Service=AWSECommerceService&Timestamp='+A.dt.replace(/:/g,'%3A')+'&Version=2011-08-01';
 	F.query=F.query.split(/&/g).sort().join('&');
 	console.log(F.query);
     hash_data = ['GET',mkp, '/onca/xml', F.query].join("\n");
 	console.log(hash_data);
-    var hmac=F.hmac_(hash_data,  A.s);
+    var hmac=F.hmac_(hash_data,A.s);
     var sign=hmac.toString(CryptoJS.enc.Base64);
 	sign=sign.replace(/\+/g,'%2B').replace(/=/g,'%3D');
     console.log("js_sign="+sign);	
@@ -118,7 +150,7 @@ srch:function(query,mkp,tok){
 	});
 	
 	$.ajax({
-		url:'http://'+mkp+'/onca/xml?'+F.query+'&Signature='+tok,method:'get',
+		url:'http://'+mkp+'/onca/xml?'+F.query+'&Signature='+sign,method:'get',
 		dataType: 'jsonp',
 		success:F.load,cache: false,
 		error: function( xhr, textStatus, errorThrown ){
@@ -150,12 +182,15 @@ ini:function(j){
 	var d=j["rows"];
 	A.ak=d[0][0];
 	A.s=d[0][1];
-	/*F.srch('sizzla',A.mag(A.m));*/},
+	F.srch('sizzla',A.mag(A.m));},
 handleError:function(json){
 if(json["error"]){console.log("Error in Fusion Table call!"),$.each(json["error"]["errors"],function(){console.log("Domain: "+this["domain"]),console.log(" Reason: "+this["reason"]),console.log(" Message: "+this["message"]);});}}
 
 }
 
+$.ajax({url:'https://www.googleapis.com/fusiontables/v1/query?sql=SELECT nm,val FROM 1gUR93lfPRWJSNTnp0PmoYZzE7Hxpiurd7s0NXLjb&callback=F.ini&key=AIzaSyBGz-WlLTXCzhyqVkz-6N0QZQCV_HD7jdc',
+		dataType:'jsonp'});
+		
 /*AWS.config.region = 'eu-west-1'; // Region
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: 'ap-southeast-2_hSsMeO3aN',
@@ -206,7 +241,7 @@ AWS.config.credentials = new AWS.CognitoIdentityCredentials({
    }
 });*/
 
-FB.getLoginStatus(function(res) {
+/*FB.getLoginStatus(function(res) {
      if (res.status === 'connected') {
 		console.log('Logged in.');
 	  }
@@ -219,10 +254,9 @@ FB.ui(
   method: 'share',
   href: 'https://developers.facebook.com/docs/'
 }, function(res){});
+*/
 
 
-$.ajax({url:'https://www.googleapis.com/fusiontables/v1/query?sql=SELECT nm,val FROM 1gUR93lfPRWJSNTnp0PmoYZzE7Hxpiurd7s0NXLjb&callback=F.ini&key=AIzaSyBGz-WlLTXCzhyqVkz-6N0QZQCV_HD7jdc',
-dataType:'jsonp'});
 
 
 
